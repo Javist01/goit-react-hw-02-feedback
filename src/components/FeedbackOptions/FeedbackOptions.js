0560bf9b0ca5410ca987c877daa614f0fa6e981e
Vlad <40580001+Javist01@users.noreply.github.com>
2022-11-11 18:46:18 +0200

    /

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ListOfButtons, Item, Button } from './FeedbackOptions.styled';
+
+export function FeedbackOptions({ onLeaveFeedback }) {
+  return (
+    <>
+      <ListOfButtons>
+        <Item>
+          <Button value="good" onClick={e => onLeaveFeedback(e)}>
+            Good
+          </Button>
+        </Item>
+        <Item>
+          <Button value="neutral" onClick={e => onLeaveFeedback(e)}>
+            Neutral
+          </Button>
+        </Item>
+        <Item>
+          <Button value="bad" onClick={e => onLeaveFeedback(e)}>
+            Bad
+          </Button>
+        </Item>
+      </ListOfButtons>
+    </>
+  );
+}
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func,
+};
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const MainSection = styled.section`
+  padding-top: 20px;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 500;
+
+  margin-bottom: 20px;
+`;
